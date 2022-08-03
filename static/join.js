2937de86326f6cd58cd9c59d3e294a0251b759a4
inwoo0808 <inwoo0808@gmail.com>
2022-08-03 17:13:06 +0900

    second commit by inwoo

---
+++ static/join.js
@@ -0,0 +1,35 @@
+  function double() {
+            $.ajax({
+                type: 'GET',
+                url: '/double',
+                data: {},
+                success: function (response) {
+                    let id = $('#userid').val()
+                    let rows = response['memberList']
+                    for (let i = 0; i < rows.length; i++) {
+                        let member_id = rows[i]['userId']
+                        if (id == member_id)
+                            alert('이미 존재하는 아이디입니다. 다시 입력하세요');
+                        if (i == rows.length - 1 && id != member_id)
+                            alert('사용가능한 아이디입니다.');
+                        if
+                            }
+                }
+            });
+        }
+
+        function register() {
+            $.ajax({
+                type: 'POST',
+                url: '/join',
+                data: {
+                    userId_give: $('#userid').val(),
+                    pw_give: $('#userpw').val(),
+                    nickname_give: $('#usernick').val(),
+                },
+                success: function (response) {
+                    window.location.reload();
+                    alert(response['msg']);
+                }
+            });
+        }
